7ff5556cd93b9ec8dbfcaacbce169dba23ecd962
hann0t <hansolivas1@gmail.com>
2022-05-13 15:38:37 -0500

    auth0 sdk

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,26 +8,39 @@ use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
-  /**
-   * The policy mappings for the application.
-   *
-   * @var array<class-string, class-string>
-   */
-  protected $policies = [
-    'App\Models\Model' => 'App\Policies\ModelPolicy',
-  ];
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
 
-  /**
-   * Register any authentication / authorization services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    $this->registerPolicies();
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
 
-    if (!$this->app->routesAreCached()) {
-      Passport::routes();
+        if (!$this->app->routesAreCached()) {
+            Passport::routes();
+        }
+
+        // Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+
+        // Passport::hashClientSecrets();
+
+        // Passport::tokensExpireIn(now()->addDays(15));
+        // Passport::refreshTokensExpireIn(now()->addDays(30));
+        // Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        // Passport::useTokenModel(Token::class);
+        // Passport::useClientModel(Client::class);
+        // Passport::useAuthCodeModel(AuthCode::class);
+        // Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
     }
-  }
 }
